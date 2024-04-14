fe72f416172ebb3d4673acb9dbb9dca7bfbbc7a1
Ben Pearson <blpearson44@icloud.com>
2024-04-13 17:06:16 -0700

    the neovim journey continues

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,30 @@
 return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+                config = function()
+                    require('telescope').setup{
+                        extensions = {
+                            fzf = {
+                                fuzzy = true,                    -- false will only do exact matching
+                                override_generic_sorter = true,  -- override the generic sorter
+                                override_file_sorter = true,     -- override the file sorter
+                                case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                            }
+                        }
+                    }
+                    require('telescope').load_extension('fzf')
+                end
+            }
+        },
         event = 'VimEnter',
         config = function()
             local builtin = require 'telescope.builtin'
+            local actions = require 'telescope.actions'
             vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
             vim.keymap.set('n', '<c-p>', builtin.git_files, {})
             vim.keymap.set('n', '<leader><leader>', builtin.buffers)
@@ -12,8 +32,25 @@ return {
             vim.keymap.set('n', '<leader>gc', builtin.git_commits)
             vim.keymap.set('n', '<leader>gf', builtin.git_bcommits)
             vim.keymap.set('n', '<leader>gs', builtin.git_status)
-            vim.keymap.set('n', '<leader>ss', builtin.grep_string)
+            vim.keymap.set('n', '<leader>ss', builtin.grep_string )
+
+
             require('telescope').setup{
+                defaults = {
+                    grep_open_files = false,
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = {
+                                actions.move_selection_next, type = "action",
+                                opts = { nowait = true, silent = true }
+                            },
+                            ["<C-k>"] = {
+                                actions.move_selection_previous, type = "action",
+                                opts = { nowait = true, silent = true }
+                            }
+                        }
+                    }
+                },
                 pickers = {
                     buffers = {
                         ignore_current_buffer = true,
