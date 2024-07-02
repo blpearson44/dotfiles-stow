b43a4f58afc1c00c3ed6863c873ca026ee2b3333
Ben Pearson <blpearson44@icloud.com>
2024-07-01 21:43:23 -0700

    indent-blankline

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+    "lukas-reineke/indent-blankline.nvim", main = "ibl",
+    opts = {
+        scope = {
+            enabled = true,
+        },
+        exclude = {
+            filetypes = {"neogit", "help", "terminal", "dashboard"},
+            buftypes = {"terminal"}
+        }
+    }
+}
