4c7a713c4307207c2d04bf34a5091b0f728dbd81
Ben Pearson <blpearson44@icloud.com>
2024-08-09 14:44:19 -0700

    markview

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
         config = function ()
             require'nvim-treesitter.configs'.setup{
                 highlight = { enable = true, additional_vim_regex_highlighting = true},
-                ensure_installed = { 'lua', 'markdown', 'markdown_inline', 'python', 'yaml', 'json', 'javascript', 'norg'},
+                ensure_installed = { 'lua', 'markdown', 'markdown_inline', 'python', 'yaml', 'json', 'javascript'},
                 auto_install = true,
                 indent = { enable = true },
             }
