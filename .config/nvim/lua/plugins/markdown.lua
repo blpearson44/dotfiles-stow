c8684354dcc094cf2760c92aaf9cba39ed1878bd
Ben Pearson <blpearson44@icloud.com>
2024-08-13 09:29:34 -0700

    pre-testing

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,7 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     ft = 'markdown',
+    version = '*',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
     opt = {
         render_modes = { 'n', 'v', 'i', 'c' }
